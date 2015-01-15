b22a4eb85c3bb45fc2f6e695e5a5b8a3a5fc1e5d
Matt Hammond <matthew@matthewhammond.net>
2015-01-15 18:52:28 +0000

    Update file.php

--- views/formr/bootstrap/3.3/file.php
+++ views/formr/bootstrap/3.3/file.php
@@ -4,7 +4,7 @@ echo Formr_Render::label($column, $options);
 echo '<div class="col-sm-10">';
 echo Form::file($column['column_name'],
 	Arr::merge(array(
-		'class' => 'form-control input-file '.$options['classes'][$column['column_name']],
+		'class' => $options['classes'][$column['column_name']],
 	), $disabled, (isset($options['attributes'][$column['column_name']]) ? $options['attributes'][$column['column_name']] : array())));
 echo (isset($options['help'][$column['column_name']]) or isset($options['errors'][$column['column_name']])) ? '<p class="help-block">'.(isset($options['errors'][$column['column_name']]) ? $options['errors'][$column['column_name']]: $options['help'][$column['column_name']]).'</p>' : '';
 echo '</div>';
